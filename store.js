2cee2a7110ace0a3f1ef9a7c212ab7b6ba0ad3c3
badkamyk <slothmateusz@gmail.com>
2022-08-12 18:31:31 +0200

    Hackernews clone, learning JS

---
+++ store.js
@@ -0,0 +1,34 @@
+function createStore(reducer) {
+   let currentState = reducer(undefined, {});
+   
+   return {
+     getState: () => currentState,
+     dispatch: action => {
+        currentState = reducer(currentState, action);    
+     } 
+   }
+}
+
+const initialState = {
+  favorites: []  
+}
+
+function favoritesReducer(state = initialState, action) {
+   switch (action.type) {
+      case "ADD_FAVORITE": {
+        const addedFavorite = action.payload.favorite;
+        const favorites = [...state.favorites, addedFavorite];
+        return { favorites };
+      }
+      case "REMOVE_FAVORITE": {
+        const removedFavorite = action.payload.favorite;
+        const favorites = state.favorites.filter(favorite => favorite.id !== removedFavorite.id);
+        return { favorites };
+      }
+      default:
+        return state;
+   } 
+}
+
+const store = createStore(favoritesReducer);
+export default store;
